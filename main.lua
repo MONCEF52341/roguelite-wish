80760101c102d952a7b965fd57a34d6659772105
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-02-23 19:24:01 +0000

    bugFix verifier les stats ne fait plus avancer

--- main.lua
+++ main.lua
@@ -11,22 +11,29 @@ local salle_actuelle = 1
 local salles_totales = 10
 
 while Player.hp > 0 and salle_actuelle <= salles_totales do
+  -- Générer la salle
   local resultat = event_generator.genererSalle(Player, salle_actuelle, salles_totales)
 
   -- Actions du joueur après chaque salle
-  print("\nQue voulez-vous faire ?")
-  print("1. Continuer")
-  print("2. Vérifier les stats")
-  print("3. Quitter le donjon")
-  local choix = tonumber(io.read())
+  local choix = nil
+  repeat
+    print("\nQue voulez-vous faire ?")
+    print("1. Continuer")
+    print("2. Vérifier les stats")
+    print("3. Quitter le donjon")
+    choix = tonumber(io.read())
 
-  if choix == 2 then
-    recap.afficherStats(Player)
-  elseif choix == 3 then
-    print("🏃 Vous quittez le donjon. À bientôt, " .. Player.name .. " !")
-    return
-  end
+    if choix == 2 then
+      recap.afficherStats(Player)
+    elseif choix == 3 then
+      print("🏃 Vous quittez le donjon. À bientôt, " .. Player.name .. " !")
+      return
+    elseif choix ~= 1 then
+      print("⛔ Choix invalide. Essayez encore.")
+    end
+  until choix == 1
 
+  -- Avancer à la salle suivante uniquement si le joueur choisit "Continuer"
   salle_actuelle = salle_actuelle + 1
 end
 
